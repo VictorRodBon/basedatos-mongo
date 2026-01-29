c6ecdea18b45153eab1ac5f28b50a2c6cc02c6e5
VictorRodBon <victorrodbon@gmail.com>
2026-01-29 09:05:52 +0100

    comentar middleware

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,8 +1,8 @@
-// Almacenamiento de imágenes en el servidor:
-
 const multer = require('multer');
 const path=require('path');
 const crypto=require('crypto');
+
+// Configuramos la ruta de almacenamiento
 
 const storage=multer.diskStorage({
   destination:(req,file,cb)=>{
@@ -15,11 +15,17 @@ const storage=multer.diskStorage({
   }
 })
 
+// Filtramos el tipo de archivo mediante su mimetype
+
 const fileFilter = (req, file, cb) => { 
   const allowed = ['image/jpeg', 'image/png', 'image/webp']; 
-  if (allowed.includes(file.mimetype)) cb(null, true); 
-  else cb(new Error('Formato no permitido'), false); 
+  if (allowed.includes(file.mimetype)){
+    cb(null, true); 
+  }else {
+    cb(new Error('Formato no permitido'), false); 
+  }
 }; 
+
 module.exports = multer({ 
   storage, fileFilter, limits: { 
     fileSize: 5 * 1024 * 1024 // 5 MB 
